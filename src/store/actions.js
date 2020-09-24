d7ec9015787d1662c7268c444acf6d9f3f13e412
oops <1909759097@qq.com>
2020-09-24 17:42:25 +0800

    完成购物车功能

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,7 +9,9 @@ import {
   RESET_USER_INFO,
   RECEIVE_GOODS,
   RECEIVE_RATINGS,
-  RECEIVE_INFO
+  RECEIVE_INFO,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 import {
   reqAddress,
@@ -92,11 +94,21 @@ export default {
     }
   },
   // 异步获取商家商品列表
-  async getShopGoods ({commit}) {
+  async getShopGoods ({commit}, cb) {
     const result = await reqShopGoods()
     if (result.code === 0) {
       const goods = result.data
       commit(RECEIVE_GOODS, {goods})
+      // 数据更新了，通知一下组件
+      cb && cb()
+    }
+  },
+  // 同步更新food中的count值
+  updateFoodCount ({commit}, {isAdd, food}) {
+    if (isAdd) {
+      commit(INCREMENT_FOOD_COUNT, {food})
+    } else {
+      commit(DECREMENT_FOOD_COUNT, {food})
     }
   }
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 /*
 直接更新 state 的多个方法的对象
  */
+import Vue from 'vue'
 import {
   RECEIVE_ADDRESS,
   RECEIVE_CATEGORYS,
@@ -9,7 +10,9 @@ import {
   RESET_USER_INFO,
   RECEIVE_INFO,
   RECEIVE_RATINGS,
-  RECEIVE_GOODS
+  RECEIVE_GOODS,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT
 } from './mutation-types'
 
 export default {
@@ -36,5 +39,27 @@ export default {
   },
   [RECEIVE_GOODS] (state, {goods}) {
     state.goods = goods
+  },
+  [INCREMENT_FOOD_COUNT] (state, {food}) {
+    if (!food.count) {
+      // food.count = 1 // 新增属性(没有数据绑定)
+      /*
+      对象，属性名，属性值
+       */
+      Vue.set(food, 'count', 1) // 让新增的属性也有数据绑定
+      // 将 food 添加到 cartFoods 中
+      state.cartFoods.push(food)
+    } else {
+      food.count++
+    }
+  },
+  [DECREMENT_FOOD_COUNT] (state, {food}) {
+    if (food.count) {
+      food.count--
+      if (food.count === 0) {
+        // 将 food 从 cartFoods 中移除
+        state.cartFoods.splice(state.cartFoods.indexOf(food), 1)
+      }
+    }
   }
 }
